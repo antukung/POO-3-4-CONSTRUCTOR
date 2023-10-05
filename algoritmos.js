b2f99d40b61b616fbd7a378dec727357f8b7537d
Antu <122485403+antukung@users.noreply.github.com>
2023-10-05 12:00:39 -0300

    aca esta de nuevo

---
+++ algoritmos.js
@@ -0,0 +1,13 @@
+import { Coche } from "./Coche.js";
+import { Hombre } from "./Hombre.js";
+
+
+const elRojo = new Coche("elAzul",1);
+const carlos = new Hombre("carlos",0);
+
+
+carlos.subirAl(elRojo);
+
+
+
+
